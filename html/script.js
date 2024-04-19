0d027d88138c76e7c32d94aee03d1985ed63e1bf
franextpl <164681536+franextpl@users.noreply.github.com>
2024-04-19 18:05:50 +0200

    Removing old files to make space for new

--- html/script.js
+++
@@ -1,9 +0,0 @@
-console.log('java script jest podłączony')
-
-//let Username = prompt("Please enter your name", "Guest");
-//if (Username != null) {
-//    document.getElementById("usernameHolder").innerHTML = Username 
-//}
-function sendFunction(){
-    console.log('wysłano wiadomość')
-}
