8f85c3d2627083239ada244e5a722a8536f31293
franextpl <franext.pl@gmail.com>
2024-04-27 13:08:25 +0200

    test upload of files

---
+++ includes/message-deleter-handler.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+    $chatid = $_POST["chatid"];
+    $messageid=$_POST["messageid"];
+    echo $chatid;
+    echo $messageid;
+
+    require_once "./dbh.inc.php";
+
+    $tableid = 'm'. $chatid;
+
+
+    try {
+        $querry2 = "DELETE FROM $tableid where lp='$messageid';";
+        $stmt2 = $pdo->prepare($querry2);
+        $stmt2->execute();
+
+
+        echo "<script>location.href='../czat-creator.php';</script>";
+        die();
+    } catch (PDOException $e) {
+        die("Querry falied" . $e->getMessage());
+    }
+
+?>
